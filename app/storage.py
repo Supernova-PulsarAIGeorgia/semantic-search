025b047e926d5ba8fdee43fcc216f956aff3ca7d
salomepulsar <salome@pulsarai.ge>
2022-10-17 00:33:46 +0400

    added image storage

--- app/storage.py
+++ app/storage.py
@@ -1,10 +1,16 @@
-from typing import Protocol, runtime_checkable
+from base64 import encode
+from typing import Protocol, runtime_checkable, Optional
 from dataclasses import dataclass
+from PIL import Image
 import json
+import copy
+import os
+import torch
 
 
 @runtime_checkable
 class Storage(Protocol):
+
     def __init__(self):
         pass
 
@@ -18,21 +24,63 @@ class Storage(Protocol):
         pass
 
 
-class ImageStorage():
-    def __init__(self, src: str):
-        pass
+@dataclass
+class StorageImage:
+    image: Image
+    encoding: torch.Tensor
+    id: Optional[int] = None
 
-    def append(self):
-        pass
+
+class ImageStorage():
+
+    def __init__(self, src: str):
+        self.src = src
+        self.n = 0
+        self.images = {}
+        self._load_images()
+
+    def _load_images(self):
+        self.n = len(os.listdir(self.src))//2
+        for id in range(self.n):
+            image = Image.open(os.path.join(self.src, str(id) + '.jpg'))
+            encoding = torch.load(os.path.join(self.src, str(id) + '.pt'))
+            self.images[id] = StorageImage(id=id, image=image, encoding=encoding)
+
+    def append(self, image: StorageImage) -> int:
+        image.id = self.n
+        self.n += 1
+        self.images[image.id] = copy.deepcopy(image)
+        # image.image.save(os.path.join(self.src, str(image.id) + 'jpg'))
+        # torch.save(image.encoding, os.path.join(self.src, str(image.id) + '.pt'))
+        return image.id
+
+    def get(self, id: int) -> StorageImage:
+        if id in self.images:
+            return self.images[id]
 
     def __iter__(self):
-        pass
+        return ImageStorageIterator(self)
 
     def __len__(self):
-        pass
+        len(self.images)
+
+
+class ImageStorageIterator:
+
+   def __init__(self, image_storage: ImageStorage):
+        self._image_storage = image_storage
+        self._index = 0
+
+   def __next__(self):
+       if self._index < len(self._image_storage.images):
+            result = self._image_storage.images[self._index]
+            self._index +=1
+            return result
+       raise StopIteration
 
 
 class TextStorage():
+
     def __init__(self, src: str):
         self.texts = json.load(open(src))
 
@@ -54,7 +102,7 @@ class TextStorageIterator:
         self._index = 0
 
    def __next__(self):
-       if self._index < len(self._text_storage.texts) :
+       if self._index < len(self._text_storage.texts):
             result = self._text_storage.texts[self._index]
             self._index +=1
             return result
